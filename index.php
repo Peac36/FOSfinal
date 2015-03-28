95bb303031dbd202d8512216667a3d69328027fb
Peac36 <peac36@abv.bg>
2015-03-28 19:52:09 +0200

    Init the project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel='stylesheet' href='css/index.css'>
+</head>
+<body>
+	<div class='mainwrapper'>
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>About</li>
+				<li>FAQ</li>
+				<li>Contact</li>
+			</ul>
+		<nav>
+	</div>
+
+</body>
+</html>
