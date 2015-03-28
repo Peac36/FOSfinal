2473c8397f7d17667a92a2057f3533b0429b9553
Peac36 <peac36@abv.bg>
2015-03-28 22:57:09 +0200

    Almost done the design

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <link rel='stylesheet' href='css/index.css'>
 </head>
 <body>
+	<!--start mainwrapper -->
 	<div class='mainwrapper'>
 		<nav>
 			<ul>
@@ -11,9 +12,162 @@
 				<li>About</li>
 				<li>FAQ</li>
 				<li>Contact</li>
-			</ul>
-		<nav>
-	</div>
 
+			</ul>
+		</nav>
+		<div class='bodyholder'>
+			<!-- start of top part of body -->
+				<div id='loren'>
+					<p>About Us</p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+					Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+					when an unknown printer took a galley of type and scrambled it to make a type spe
+					cimen book.</br>
+					<span>more..<span>
+					</div>
+			<!-- end of top part of body -->
+
+
+
+		</div>
+			<!-- start of middle part of body -->
+			<div class='middle'>
+				<div class='leftcol'>
+					<div class='holder'>
+						<div class='theader'><div>Result</div></div>
+						<div class='tbody'>
+							<table id='result'>
+								<tr>
+									<th>Make</th>
+									<th>Model</th>
+									<th>Reg.Number</th>
+									<th>Power</th>
+									<th>Color</th>
+								</tr>
+								<tr>
+									<td>1</td>
+									<td>2</td>
+									<td>3</td>
+									<td>4</td>
+									<td>5</td>
+								</tr>
+							</table>
+						</div>
+						<div class='tfooter'></div>
+					</div>
+
+
+				</div>
+
+				<div class='rightcol'>
+					<div class='rightbody'>
+							<form>
+								<!-- start of first row -->
+								<div class='row'>
+
+									<div class='cell'>
+									<div>Make</div>
+									<select>
+										<option value='1'>1</option>
+									</select>
+									</div>
+
+									<div class='cell'>
+									<div>Model</div>
+									<select>
+										<option value='1'>1</option>
+									</select>
+									</div>
+									<div class='clear'></div>
+								</div>
+								<!-- end of first row -->
+
+								<!-- start of second row -->
+								<div class='row'>
+
+									<div class='cell'>
+									<div>Registration Number</div>
+									<select>
+										<option value='1'>1</option>
+									</select>
+									</div>
+
+									<div class='cell'>
+									<div>Power</div>
+									<select>
+										<option value='1'>1</option>
+									</select>
+									</div>
+									<div class='clear'></div>
+								</div>
+								<!-- end of second row -->
+
+								<!-- start of third row -->
+								<div class='row'>
+
+									<div class='cell'>
+									<div>Color</div>
+									<select>
+										<option value='1'>1</option>
+									</select>
+									</div>
+
+									<div class='cell'>
+									<div>Type</div>
+									<select>
+										<option value='1'>1</option>
+									</select>
+									</div>
+									<div class='clear'></div>
+								</div>
+								<!-- end of third row -->
+
+								<!-- start of fourth row -->
+								<div class='row'>
+
+									<div class='cell'>
+									<div>From</div>
+									<select>
+										<option value='1'>1</option>
+									</select>
+									</div>
+
+									<div class='cell'>
+									<div>To</div>
+									<select>
+										<option value='1'>1</option>
+									</select>
+									</div>
+									<div class='clear'></div>
+								</div>
+								<!-- end of fourth row -->
+
+								<div class='row'>
+
+									<div class='cell'>
+
+									</div>
+
+									<div class='cell'>
+									<button  type='button' id='search'>Search</button>
+									</div>
+									<div class='clear'></div>
+								</div>
+
+
+
+
+							</form>
+					</div>
+				</div>
+			</div>
+			<div class='clear'></div>
+			<!-- end of middle part of body -->
+		<footer>
+			<span>All right reserved solutions</span>
+		</footer>
+
+	</div>
+	<!--end mainwrapper -->
 </body>
 </html>
